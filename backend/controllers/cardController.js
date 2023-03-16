c570181b7c5cbf4e5efc552e8dcb524028781493
Karol Kijowski <kkijowski11@gmail.com>
2023-03-16 21:09:30 +0100

    error handling

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -1,42 +1,44 @@
+const asyncHandler = require('express-async-handler')
+
 /**    
  *  @desc Get cards
  *  @route GET /api/cards
  *  @access Private
 */
-const getCards = (req, res) => {
+const getCards = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get cards' })
-}
+})
 
 /**    
  *  @desc Set card
  *  @route POST /api/cards
  *  @access Private
 */
-const setCard = (req, res) => {
+const setCard = asyncHandler(async (req, res) => {
+    if (!req.body.frontCardText) {
+        res.status(400)
+        throw new Error("Add a front card text")
+    }
     res.status(200).json({ message: 'Set card' })
-}
+})
 
 /**    
  *  @desc Update card
  *  @route PUT /api/cards/:id
  *  @access Private
 */
-const updateCard = (req, res) => {
+const updateCard = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Update card' })
-}
+})
 
 /**    
  *  @desc Delete card
  *  @route DELETE /api/cards/:id
  *  @access Private
 */
-const deleteCard = (req, res) => {
+const deleteCard = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Delete card' })
-}
-
-
-
-
+})
 
 module.exports = {
     getCards,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT
+const errorHandler = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/cards', require('./routes/cardRoutes'))
 
