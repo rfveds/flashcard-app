f9505fe29dbd20620bae5413fe96088411dbd5f6
Karol Kijowski <kkijowski11@gmail.com>
2023-03-17 14:18:33 +0100

    card model

--- backend/models/cardModel.js
+++ backend/models/cardModel.js
@@ -1,35 +1,35 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const cardSchema = new mongoose.Schema({
-//   question: {
-//     type: String,
-//     required: true
-//   },
-//   answer: {
-//     type: String,
-//     required: true
-//   },
-//   easy: {
-//     type: Number,
-//     default: 0
-//   },
-//   medium: {
-//     type: Number,
-//     default: 0
-//   },
-//   hard: {
-//     type: Number,
-//     default: 0
-//   },
-//   priority: {
-//     type: Number,
-//     default: 0
-//   },
-//   deck: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Deck',
-//     required: true
-//   }
-// });
+const cardSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String,
+    required: true
+  },
+  easy: {
+    type: Number,
+    default: 0
+  },
+  medium: {
+    type: Number,
+    default: 0
+  },
+  hard: {
+    type: Number,
+    default: 0
+  },
+  priority: {
+    type: Number,
+    default: 0
+  },
+  deck: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Deck',
+    required: true
+  }
+});
 
-// module.exports = mongoose.model('Card', cardSchema);
+module.exports = mongoose.model('Card', cardSchema);
