51f0e0a52be3caddae086b781884738d22d2c522
Karol Kijowski <kkijowski11@gmail.com>
2023-03-17 10:24:28 +0100

    deck model

---
+++ backend/model/deckModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const deckSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  cards: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Card'
+  }]
+});
+
+module.exports = mongoose.model('Deck', deckSchema);
