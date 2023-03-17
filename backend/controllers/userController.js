95b56a2bed525df2403dfd61a4f8a10930764250
Karol Kijowski <kkijowski11@gmail.com>
2023-03-17 12:38:09 +0100

    jwt token login and register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,7 +18,8 @@ const loginUser = asyncHandler(async (req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -60,10 +61,11 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if (user) {
-        res.status(200).json({
+        res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -79,6 +81,16 @@ const registerUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data display' })
 })
+
+
+/**
+ * Generate JWT Token
+ */
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
